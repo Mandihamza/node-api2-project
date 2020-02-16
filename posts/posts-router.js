0f3c75ba1715a3eeb13f6fc862d7742dd5813b37
mandihamza <mandihamza@gmail.com>
2020-02-16 14:06:25 -0700

    Adds POST endpoint for new posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -55,4 +55,20 @@ router.get('/:id/comments', (req, res) => {
     })
 })
 
+//POST new post
+router.post('/', (req, res) => {
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        db.insert(req.body)
+    .then(posts => {
+        res.status(201).json(posts)
+    }).catch((err) => {
+        res.status(500).json({ 
+            error: "There was an error while saving the post to the database" })
+    });
+    }
+})
+
 module.exports = router
