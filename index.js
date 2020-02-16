b136451cfc3ab8b16ee10810885d0bf5d708ed3e
mandihamza <mandihamza@gmail.com>
2020-02-16 12:43:37 -0700

    Adds import for /posts express router

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express")
-const blog = require("./data/db.js")
 const welcomeRouter = require("./welcome/welcome-router")
+const postsRouter = require("./posts/posts-router")
 
 const server = express()
 const port =  4000
 
 server.use(express.json())
 server.use("/", welcomeRouter )
+server.use("/api/posts", postsRouter 
+)
+
 
 server.listen(port, () => {
     console.log(`server started at http://localhost:${port}`)
