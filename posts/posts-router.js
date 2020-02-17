b0317683a3ecff07e43d7812bbcd2ba0b672d4a4
mandihamza <mandihamza@gmail.com>
2020-02-17 15:16:19 -0700

    Adds new put post endpoint

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -120,4 +120,30 @@ router.delete("/:id", (req, res) => {
 		})
 })
 
+// PUT /api/posts/:id
+router.put("/:id", (req, res) => {
+	if (!req.body.title || !req.body.contents) {
+		return res.status(400).json({
+			message: "Missing post title and contents",
+		})
+	}
+
+	db.update(req.params.id, req.body)
+		.then((posts) => {
+			if (posts) {
+				res.status(200).json(posts)
+			} else {
+				res.status(404).json({
+					message: "The post could not be found",
+				})
+			}
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({
+				message: "Error updating the post",
+			})
+		})
+})
+
 module.exports = router
